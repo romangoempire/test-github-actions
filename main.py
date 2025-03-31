6ae6d364129fc14159f5e151ee205efc606b6ab4
RomanGoEmpire <71276897+RomanGoEmpire@users.noreply.github.com>
2025-03-31 08:57:30 +0200

    fix st.info display

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ with open(os.getenv("SECRET_NAME_FILE")) as f:
 st.write("Okay wow now on the other vps")
 
 
-st.info("This comes straight from github secret: ", os.getenv("GITHUB_SECRET"))
+st.info(f"This comes straight from github secret: {os.getenv("GITHUB_SECRET")}")
